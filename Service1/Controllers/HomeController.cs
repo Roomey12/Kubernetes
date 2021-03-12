bafc1a3ec2392ef06c5806ddc0fe7daee36182d1
Oleksandr Lutsenko1 <Oleksandr_Lutsenko1@epam.com>
2021-03-12 11:40:42 +0200

    init

---
+++ Service1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Service1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public HomeController() { }
+
+        [HttpGet]
+        public string GetHello()
+        {
+            return "hello from service1";
+        }
+    }
+}
