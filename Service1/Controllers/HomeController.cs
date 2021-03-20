bb24787ec2217cca75a76bbd08a4fbdb050a6b5c
SimonMansoury <saymon3228@gmail.com>
2021-03-20 10:36:14 +0200

    Lab 2 implementation

--- Service1/Controllers/HomeController.cs
+++ Service1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Service1.Controllers
 {
@@ -10,12 +11,22 @@ namespace Service1.Controllers
     [Route("[controller]")]
     public class HomeController : ControllerBase
     {
-        public HomeController() { }
+        public HomeController()
+        {
+        }
 
         [HttpGet]
         public string GetHello()
         {
             return "hello from service1";
+        }
+
+        [HttpGet]
+        [Route("TimeOutRequest")]
+        public string GetTimeOutResponse([FromQuery] int timeout = 0)
+        {
+            Thread.Sleep(timeout);
+            return $"Response was sent for the {timeout} delay.";
         }
     }
 }
