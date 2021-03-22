26b6bbdc701d3e0705b2b9dcb39b8ba5b65284af
SimonMansoury <saymon3228@gmail.com>
2021-03-22 19:34:46 +0200

    Create interservice communication

--- Service1/Controllers/HomeController.cs
+++ Service1/Controllers/HomeController.cs
@@ -2,8 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Threading;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Service1.Controllers
 {
@@ -11,9 +16,14 @@ namespace Service1.Controllers
     [Route("[controller]")]
     public class HomeController : ControllerBase
     {
-        public HomeController()
+        public HomeController(IServiceProvider serviceProvider)
         {
+            ServiceProvider = serviceProvider;
         }
+
+        static readonly HttpClient client = new HttpClient();
+
+        public IServiceProvider ServiceProvider { get; }
 
         [HttpGet]
         public string GetHello()
@@ -21,12 +31,32 @@ namespace Service1.Controllers
             return "hello from service1";
         }
 
+
         [HttpGet]
         [Route("TimeOutRequest")]
         public string GetTimeOutResponse([FromQuery] int timeout = 0)
         {
             Thread.Sleep(timeout);
             return $"Response was sent for the {timeout} delay.";
+        }
+
+        [HttpGet]
+        [Route("Ports")]
+        public IActionResult GetPorts()
+        {
+            var server = ServiceProvider.GetRequiredService<IServer>();
+            var addressFeature = server.Features.Get<IServerAddressesFeature>();
+            return Ok(addressFeature.Addresses);
+        }
+
+        [HttpGet]
+        [Route("CallService2")]
+        public async Task<IActionResult> CallService1([FromQuery] string service2url)
+        {
+            if (string.IsNullOrEmpty(service2url)) return BadRequest("Empty path");
+
+            var response = await client.GetAsync(service2url + "/Snus");
+            return Ok($"Call successful. \n Result: {await response.Content.ReadAsStringAsync()}");
         }
     }
 }
--- Service2/Controllers/SnusController.cs
+++ Service2/Controllers/SnusController.cs
@@ -2,7 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Service1.Controllers
 {
@@ -10,12 +14,38 @@ namespace Service1.Controllers
     [Route("[controller]")]
     public class SnusController : ControllerBase
     {
-        public SnusController() { }
+        private readonly IEnumerable<string> _service1Ports = new List<string>();
+        private readonly IServiceProvider _serviceProvider;
+        static readonly HttpClient client = new HttpClient();
+
+        public SnusController(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
 
         [HttpGet]
         public string GetHello()
         {
             return "hello from service2";
+        }
+
+        [HttpGet]
+        [Route("Ports")]
+        public IActionResult GetPorts()
+        {
+            var server = _serviceProvider.GetRequiredService<IServer>();
+            var addressFeature = server.Features.Get<IServerAddressesFeature>();
+            return Ok(addressFeature.Addresses);
+        }
+
+        [HttpGet]
+        [Route("CallService1")]
+        public async Task<IActionResult> CallService1([FromQuery] string service1url)
+        {
+            if (string.IsNullOrEmpty(service1url)) return BadRequest("Empty path");
+
+            var response = await client.GetAsync(service1url + "/Home");
+            return Ok($"Call successful. \n Result: {await response.Content.ReadAsStringAsync()}");
         }
     }
 }
